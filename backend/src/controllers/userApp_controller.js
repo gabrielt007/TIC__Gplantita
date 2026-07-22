35c5b130ea90ac84d00c7dcdcacaa4b75e3828c3
lubswer <lubswer@hotmail.com>
2026-07-22 18:08:38 -0500

    cambio

--- backend/src/controllers/userApp_controller.js
+++ backend/src/controllers/userApp_controller.js
@@ -4,62 +4,62 @@ import { crearTokenJWT } from "../middleware/JWT.js"
 import mongoose from "mongoose"
 
 const registro = async (req, res) => {
-    try{
-        const {email, password, nombre, apellido, celular} = req.body;
-        if(Object.values(req.body).includes("") || !email || !password || !nombre || !apellido){
-            return res.status(400).json({msg: "Lo sentimos, debes llenar todos los campos requeridos"})
+    try {
+        const { email, password, nombre, apellido, celular } = req.body;
+        if (Object.values(req.body).includes("") || !email || !password || !nombre || !apellido) {
+            return res.status(400).json({ msg: "Lo sentimos, debes llenar todos los campos requeridos" })
         }
 
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
-            return res.status(400).json({msg: "El formato del correo electrónico no es válido"})
+            return res.status(400).json({ msg: "El formato del correo electrónico no es válido" })
         }
 
         const nameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
         if (!nameRegex.test(nombre) || !nameRegex.test(apellido)) {
-            return res.status(400).json({msg: "El nombre y apellido solo pueden contener letras"})
+            return res.status(400).json({ msg: "El nombre y apellido solo pueden contener letras" })
         }
-        if(celular.length !== 10){
+        if (celular.length !== 10) {
 
-            return res.status(400).json({ msg : "El número de celular debe contener exáctamente 10 dígitos"})           
+            return res.status(400).json({ msg: "El número de celular debe contener exáctamente 10 dígitos" })
         }
         if (password.length < 8) {
-            return res.status(400).json({msg: "La contraseña debe tener al menos 8 caracteres"})
+            return res.status(400).json({ msg: "La contraseña debe tener al menos 8 caracteres" })
         }
 
 
         const verificarEmailBDD = await userApp.findOne({ email });
 
-        if(verificarEmailBDD){
-            return res.status(400).json({ msg : "Lo sentimos, el email ya se encuentra registrado"})
+        if (verificarEmailBDD) {
+            return res.status(400).json({ msg: "Lo sentimos, el email ya se encuentra registrado" })
         }
         const nuevoUserApp = new userApp(req.body)
         nuevoUserApp.password = await nuevoUserApp.encryptPassword(password)
         const token = nuevoUserApp.createToken();
         await sendMailToRegister(email, token);
         await nuevoUserApp.save();
-        res.status(200).json({ msg : "Revisa tu correo electrónico para confirmar tu cuenta"})
+        res.status(200).json({ msg: "Revisa tu correo electrónico para confirmar tu cuenta" })
 
-    }catch (error){
-        res.status(500).json({msg: `Error en el servidor - ${error}`});
+    } catch (error) {
+        res.status(500).json({ msg: `Error en el servidor - ${error}` });
     }
 }
 
-const confirmarMail = async (req, res) =>{
-    try{
-        const  { token } = req.params
+const confirmarMail = async (req, res) => {
+    try {
+        const { token } = req.params
         const userAppBDD = await userApp.findOne({ token })
-        if(!userAppBDD){
-            return res.status(404).json({ msg: "Token inválido o cuenta ya confirmada"})
+        if (!userAppBDD) {
+            return res.status(404).json({ msg: "Token inválido o cuenta ya confirmada" })
         }
         userAppBDD.token = null
         userAppBDD.confirmEmail = true
         await userAppBDD.save();
 
-        res.status(200).json({msg: "Cuenta confirmada, ya puedes inciar sesión"})
-    }catch(error){
-        res.status(500).json({msg: `Error en el servidor - ${error}`})
+        res.status(200).json({ msg: "Cuenta confirmada, ya puedes inciar sesión" })
+    } catch (error) {
+        res.status(500).json({ msg: `Error en el servidor - ${error}` })
     }
 }
 
@@ -79,27 +79,7 @@ const recuperarPassword = async (req, res) => {
         await userAppBDD.save()
         // Paso 4
         res.status(200).json({ msg: "Revisa tu correo electrónico para reestablecer tu cuenta" })
-        
-    } catch (error) {
-    console.error(error)
-        res.status(500).json({ msg: `❌ Error en el servidor - ${error}` })
-    }
-}
 
-
-
-const comprobarTokenPasword = async (req,res)=>{
-    try {
-        // Paso 1
-        const {token} = req.params
-        // Paso 2
-        const userAppBDD = await userApp.findOne({token})
-        if(userAppBDD?.token !== token) return res.status(404).json({msg:"Lo sentimos, no se puede validar la cuenta"})
-        // Paso 3
-
-        // Paso 4
-        res.status(200).json({msg:"Token confirmado, ya puedes crear tu nuevo password"}) 
-    
     } catch (error) {
         console.error(error)
         res.status(500).json({ msg: `❌ Error en el servidor - ${error}` })
@@ -108,23 +88,43 @@ const comprobarTokenPasword = async (req,res)=>{
 
 
 
-const crearNuevoPassword = async (req,res)=>{
+const comprobarTokenPasword = async (req, res) => {
+    try {
+        // Paso 1
+        const { token } = req.params
+        // Paso 2
+        const userAppBDD = await userApp.findOne({ token })
+        if (userAppBDD?.token !== token) return res.status(404).json({ msg: "Lo sentimos, no se puede validar la cuenta" })
+        // Paso 3
+
+        // Paso 4
+        res.status(200).json({ msg: "Token confirmado, ya puedes crear tu nuevo password" })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ msg: `❌ Error en el servidor - ${error}` })
+    }
+}
+
+
+
+const crearNuevoPassword = async (req, res) => {
 
     try {
         // Paso 1
-        const{password,confirmpassword} = req.body
+        const { password, confirmpassword } = req.body
         const { token } = req.params
         // Paso 2
-        if (Object.values(req.body).includes("") || !password || !confirmpassword) return res.status(404).json({msg:"Debes llenar todos los campos"})
-        if(password !== confirmpassword) return res.status(404).json({msg:"Los passwords no coinciden"})
-        const userAppBDD = await userApp.findOne({token})
-        if(!userAppBDD) return res.status(404).json({msg:"No se puede validar la cuenta"})
+        if (Object.values(req.body).includes("") || !password || !confirmpassword) return res.status(404).json({ msg: "Debes llenar todos los campos" })
+        if (password !== confirmpassword) return res.status(404).json({ msg: "Los passwords no coinciden" })
+        const userAppBDD = await userApp.findOne({ token })
+        if (!userAppBDD) return res.status(404).json({ msg: "No se puede validar la cuenta" })
         // Paso 3
         userAppBDD.token = null
         userAppBDD.password = await userAppBDD.encryptPassword(password)
         await userAppBDD.save()
         // Paso 4
-        res.status(200).json({msg:"Felicitaciones, ya puedes iniciar sesión con tu nuevo password"}) 
+        res.status(200).json({ msg: "Felicitaciones, ya puedes iniciar sesión con tu nuevo password" })
 
     } catch (error) {
         console.error(error)
@@ -132,24 +132,24 @@ const crearNuevoPassword = async (req,res)=>{
     }
 }
 
-const login = async(req,res)=>{
+const login = async (req, res) => {
 
     try {
         // Paso 1
-        const {email,password} = req.body
+        const { email, password } = req.body
         // Paso 2
-        if (Object.values(req.body).includes("") || !email || !password) return res.status(404).json({msg:"Debes llenar todos los campos"})
+        if (Object.values(req.body).includes("") || !email || !password) return res.status(404).json({ msg: "Debes llenar todos los campos" })
 
-        const userAppBDD = await userApp.findOne({email}).select("-status -__v -token -updatedAt -createdAt")
+        const userAppBDD = await userApp.findOne({ email }).select("-status -__v -token -updatedAt -createdAt")
 
-        if(!userAppBDD) return res.status(404).json({msg:"El usuario no se encuentra registrado"})
+        if (!userAppBDD) return res.status(404).json({ msg: "El usuario no se encuentra registrado" })
 
-        if(!userAppBDD.confirmEmail) return res.status(403).json({msg:"Debes verificar tu cuenta antes de iniciar sesión"})
+        if (!userAppBDD.confirmEmail) return res.status(403).json({ msg: "Debes verificar tu cuenta antes de iniciar sesión" })
         const verificarPassword = await userAppBDD.matchPassword(password)
-        if(!verificarPassword) return res.status(401).json({msg:"El password no es correcto"})
+        if (!verificarPassword) return res.status(401).json({ msg: "El password no es correcto" })
         // Paso 3
-        const {nombre,apellido,direccion,celular,_id,rol} = userAppBDD
-        const token = crearTokenJWT(userAppBDD._id,userAppBDD.rol)
+        const { nombre, apellido, direccion, celular, _id, rol } = userAppBDD
+        const token = crearTokenJWT(userAppBDD._id, userAppBDD.rol)
 
         // Paso 4
         res.status(200).json({
@@ -170,31 +170,31 @@ const login = async(req,res)=>{
     }
 }
 
-const perfil =(req,res)=>{
-	const {token,confirmEmail,createdAt,updatedAt,__v,...datosPerfil} = req.userAppHeader
+const perfil = (req, res) => {
+    const { token, confirmEmail, createdAt, updatedAt, __v, ...datosPerfil } = req.userAppHeader
     res.status(200).json(datosPerfil)
 }
 
-const actualizarPerfil = async (req,res)=>{
+const actualizarPerfil = async (req, res) => {
 
     try {
-        const {id} = req.params
-        const {nombre,apellido,direccion,celular} = req.body
-        if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(400).json({msg:`ID inválido: ${id}`})
+        const { id } = req.params
+        const { nombre, apellido, direccion, celular } = req.body
+        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(400).json({ msg: `ID inválido: ${id}` })
         const userAppBDD = await userApp.findById(id)
-        if(!userAppBDD) return res.status(404).json({ msg: `No existe el Usuario con ID ${id}` })
-        if (Object.values(req.body).includes("") || !nombre || !apellido) return res.status(400).json({msg:"Debes llenar todos los campos requeridos"})
+        if (!userAppBDD) return res.status(404).json({ msg: `No existe el Usuario con ID ${id}` })
+        if (Object.values(req.body).includes("") || !nombre || !apellido) return res.status(400).json({ msg: "Debes llenar todos los campos requeridos" })
 
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
         const nameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
         if (!nameRegex.test(nombre) || !nameRegex.test(apellido)) {
-            return res.status(400).json({msg: "El nombre y apellido solo pueden contener letras"})
+            return res.status(400).json({ msg: "El nombre y apellido solo pueden contener letras" })
         }
-        if(celular.length !== 10){
+        if (celular.length !== 10) {
 
-            return res.status(400).json({ msg : "El número de celular debe contener exáctamente 10 dígitos"})           
+            return res.status(400).json({ msg: "El número de celular debe contener exáctamente 10 dígitos" })
         }
 
 
@@ -205,7 +205,7 @@ const actualizarPerfil = async (req,res)=>{
 
         await userAppBDD.save()
         res.status(200).json(userAppBDD)
-        
+
     } catch (error) {
         console.error(error)
         res.status(500).json({ msg: `❌ Error en el servidor - ${error}` })
@@ -215,24 +215,24 @@ const actualizarPerfil = async (req,res)=>{
 const actualizarPassword = async (req, res) => {
     try {
 
-        const {id} = req.params
-        const {passwordActual, confirmPassword, repeatComfirmPassword} = req.body
+        const { id } = req.params
+        const { passwordActual, confirmPassword, repeatComfirmPassword } = req.body
 
-        if(Object.values(req.body).includes("") || !passwordActual || !confirmPassword || !repeatComfirmPassword) return res.status(404).json({msg:"Debe de llenar todos los campos"})
-        if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({msg:"El id no es valido"})
+        if (Object.values(req.body).includes("") || !passwordActual || !confirmPassword || !repeatComfirmPassword) return res.status(404).json({ msg: "Debe de llenar todos los campos" })
+        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ msg: "El id no es valido" })
         const userAppBDD = await userApp.findById(id)
-        if(!userAppBDD) return res.status(404).json({msg:`No existe el usuario con ID ${id}`})
+        if (!userAppBDD) return res.status(404).json({ msg: `No existe el usuario con ID ${id}` })
         if (confirmPassword.length < 8) {
-            return res.status(400).json({msg: "La contraseña debe tener al menos 8 caracteres"})
+            return res.status(400).json({ msg: "La contraseña debe tener al menos 8 caracteres" })
         }
         const verificarPassword = await userAppBDD.matchPassword(passwordActual)
-        if(!verificarPassword) return res.status(404).json({msg:`Lo sentimos, el password actual no es el correcto`})
-        if(confirmPassword !== repeatComfirmPassword) return res.status(404).json({msg:`Los password no coinciden`})
+        if (!verificarPassword) return res.status(404).json({ msg: `Lo sentimos, el password actual no es el correcto` })
+        if (confirmPassword !== repeatComfirmPassword) return res.status(404).json({ msg: `Los password no coinciden` })
         userAppBDD.password = await userAppBDD.encryptPassword(confirmPassword)
-        await userAppBDD.save() 
-        res.status(200).json({msg:`Password Actualizado Correctamente`})
+        await userAppBDD.save()
+        res.status(200).json({ msg: `Password Actualizado Correctamente` })
     } catch (error) {
-        res.status(500).json({ msg: `❌ Error en el servidor - ${error} `})
+        res.status(500).json({ msg: `❌ Error en el servidor - ${error} ` })
     }
 }
 
